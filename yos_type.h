c5dfafd49c0a7c63f12fe04aa9e807d52d91fa39
MarioGuyWasTakenAgain <135893416+MarioGuyWasTakenAgain@users.noreply.github.com>
2023-08-14 13:30:06 -0400

    add source

---
+++ yos_type.h
@@ -0,0 +1,26 @@
+//======================================================
+//    yos_main.c
+//    Sample Yoshi Pattern Definition Header
+//
+//    Copyright (C) 1999-2001 NINTENDO Co.,Ltd.
+//======================================================
+#ifndef YOS_TYPE_H
+#define YOS_TYPE_H
+
+typedef struct xyPos_tag
+{
+	s32 x;
+	s32 y;
+}xyPos;
+typedef struct s8xyPos_tag
+{
+	s8 x;
+	s8 y;
+}s8xyPos;
+typedef struct s16xyPos_tag
+{
+	s16 x;
+	s16 y;
+}s16xyPos;
+
+#endif
